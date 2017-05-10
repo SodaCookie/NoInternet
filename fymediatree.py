bcc90f32891b84540169b663128ba5cea54df499
Eric Zhang <eric.quin.zhang@gmail.com>
2017-05-10 02:54:54 -0400

    Organized fymedia tree, added wav, implemented testing

--- fymediatree.py
+++ fymediatree.py
@@ -1,16 +1,33 @@
 """Defines the decision tree for FY media"""
 
-Welcome to FYMedia. A Reseller of CUX Cable.
-Please select your 3 preferred install dates from the list below:
-1 -> [Current Date + 10]
-2 -> [Current Date + 11]
-3 -> [Current Date + 12]
---Input--
-Please select your preferred install block...
-1 -> 6am - 12pm
-2 -> 1pm - 12am
---Input--
-Thank you for choosing FYMedia. We will get back to you shortly confirming the date of your install!
---Hangup--
---Time passes--
-Your install date has been confirmed for [Current Date + 12] from 1pm - 12am. A CUX representative will come to install. Have a nice day!
+fymediatree = {}
+
+fymediatree["main"] = {
+    "message" : [
+        "Welcome to FYMedia. A Reseller of CUX Cable.",
+        "Please select your 3 preferred install dates from the list below:",
+        "1 -> {day + 10}",
+        "2 -> {day + 11}",
+        "3 -> {day + 12}"
+    ],
+    "choices" : ["block", "block", "block"]
+}
+
+fymediatree["block"] = {
+    "message" = [
+        "Please select your preferred install block...",
+        "1 -> 6am - 12pm",
+        "2 -> 1pm - 12am"
+    ],
+    "choices" = ["complete", "complete"]
+}
+
+fymediatree["complete"] = {
+    "message" = [
+        "Thank you for choosing FYMedia. We will get back to you shortly confirming the date of your install!"
+    ],
+    "choices" = []
+}
+
+#--Time passes--
+"Your install date has been confirmed for {day + 12} from {1pm - 12am}. A CUX representative will come to install. Have a nice day!"
--- nointernet.py
+++ nointernet.py
@@ -3,7 +3,15 @@ import time
 import textwrap
 import curses.textpad
 import curses.ascii
+import pygame
+import random
+import sched
 
+
+# Globals
+scheduler = sched.scheduler(time.time, time.sleep)
+terminal_on = True # If on we have default terminal; if off we get free mode
+phone_on = False # If on we append random messages to the current_message
 
 def typeout(stdscr, string, y, x, delay=50):
     i = 0
@@ -12,6 +20,24 @@ def typeout(stdscr, string, y, x, delay=50):
         i += 1
         curses.napms(delay)
         stdscr.refresh()
+
+def stop_music():
+    pygame.mixer.music.stop()
+    get_input = True
+    on_phone = False
+
+def start_call():
+    def call():
+        pass
+
+    # Start the music
+    pygame.mixer.music.load("elevator.wav")
+    pygame.mixer.music.play()
+    delay = random.randrange(10, 20)
+    scheduler.enter(delay, 1, stop_music, ())
+    scheduler.enter(delay + 0.5, 1, call, ())
+    get_input = False
+    on_phone = True
 
 def introduction(stdscr):
     height, width = stdscr.getmaxyx()
@@ -34,6 +60,7 @@ def input_validator(key):
     return key
 
 def main(stdscr):
+    global terminal_on, phone_on
     # Setup
     stdscr.clear()
     height, width = stdscr.getmaxyx()
@@ -42,6 +69,7 @@ def main(stdscr):
     # Post introduction setup
     stdscr.nodelay(1)
     curses.noecho()
+    pygame.mixer.init()
 
     # Create text windows
     text_window = curses.newwin(38, 82, height // 2 - 21, width // 2 - 41)
@@ -55,7 +83,6 @@ def main(stdscr):
     running = True
     days = 0
     user_input = ""
-    get_input = True
     current_message = ["You moved to a new apartment complex and have no access to internet. But fear not, you are a competent, young adult! There's nothing you can't handle with a bit of resolve and patience. Your goal is to get your internet installed by FYMedia.", "", "Type 'help' for a list of commands."]
     options = []
 
@@ -68,43 +95,63 @@ def main(stdscr):
                 # Quit all recieving input and quit
                 running = False
                 get_input = False
-            elif command[0] == "help":
-                current_message = [
-                    "lookup [thing] - Find out information about a thing",
-                    "call [phone number] - Calls the company.",
-                    "wait - Wait one day.",
-                    "days - Returns number of days past since no internet.",
-                    "exit/quit - Quit the game"
-                ]
-            elif command[0] == "lookup":
-                if len(command) > 1:
-                    query = " ".join(command[1:]).strip()
-                    if query == "fymedia":
-                        current_message = [
-                            "Luckily you were smart enough to write down the contact info of FYMedia before you didn't have access to internet. Scribbled on the back of a unopenned envelope you see:",
-                            "",
-                            "Phone: (993) 273-6782",
-                            "Website: www.fymediaconnect.com",
-                        ]
-                    elif query == "cux" or query == "cux cable":
-                        current_message = [
-                            "You recall the website of CUX Cable but don't have the phone number on hand:",
-                            "",
-                            "Website: www.thecuxprovider.com"
-                        ]
-                    elif query == "sodacookie":
-                        current_message = [
-                            "The God of Curses."
-                        ]
+            if terminal_on:
+                if command[0] == "test":
+                    start_call()
+                    current_message = ["Testing..."]
+                elif command[0] == "help":
+                    current_message = [
+                        "lookup [thing] - Find out information about a thing",
+                        "call [phone number] - Calls the company.",
+                        "wait - Wait one day.",
+                        "days - Returns number of days past since no internet.",
+                        "back - restore the previous message.",
+                        "exit/quit - Quit the game."
+                    ]
+                elif command[0] == "lookup":
+                    if len(command) > 1:
+                        query = " ".join(command[1:]).strip()
+                        if query == "fymedia":
+                            current_message = [
+                                "Luckily you were smart enough to write down the contact info of FYMedia before you didn't have access to internet. Scribbled on the back of a unopenned envelope you see:",
+                                "",
+                                "Phone: (993) 273-6782",
+                                "Website: www.fymediaconnect.com",
+                            ]
+                        elif query == "cux" or query == "cux cable":
+                            current_message = [
+                                "You recall the website of CUX Cable but don't have the phone number on hand:",
+                                "",
+                                "Website: www.thecuxprovider.com"
+                            ]
+                        elif query == "sodacookie":
+                            current_message = [
+                                "The God of Curses."
+                            ]
+                        else:
+                            current_message = ["You can't seem to recall or find any information on %s." % query]
                     else:
-                        current_message = ["You can't seem to recall or find any information on %s." % query]
+                        current_message = ["The 'lookup' command requires one argument.", "", "Please try again or type 'help' for a list of commands."]
+                elif command[0] == "call":
+                    if len(command) > 1:
+                        # Normalize all numbers
+                        query = "".join(command[1:])
+                        query = query.replace("-", "").replace("(", "").replace(")", "")
+                        if query == "9932736782":
+                            pass
+                        elif query.isnumeric():
+                            current_message = ["The number you have dialed is currently unavailable. Ensure that you have typed the correct number then hangup and redial the number.", "", "The phone display shows '%s'" % query]
+                        else:
+                            current_message = ["That's not a number."]
+                    else:
+                        current_message = ["The 'call' command requires a phone number.", "", "Please try again or type 'help' for a list of commands."]
                 else:
-                    current_message = ["The 'lookup' command requires one argument.", "", "Please try again or type 'help' for a list of commands."]
+                    # Default if missing info
+                    current_message = ["'%s' is an unknown command." % user_input.split()[0], "", "Please try again or type 'help' for a list of commands."]
             else:
-                # Default if missing info
-                current_message = ["'%s' is an unknown command." % user_input.split()[0], "", "Please try again or type 'help' for a list of commands."]
-
+                current_message.append(user_input)
         if current_message:
+            # TODO ADD HEIGHT PROTECTION
             text_window.erase()
             h = 0
             for message in current_message:
@@ -126,6 +173,9 @@ def main(stdscr):
         curses.doupdate()
 
         # Handle text events after displaying screen
-        if get_input:
+        if terminal_on:
             user_input = textbox.edit(input_validator).lower().strip()
             input_window.erase()
+        if phone_on:
+            # TODO implement random messages
+            pass
